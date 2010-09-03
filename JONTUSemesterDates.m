05360d1dd920d99a3047db9410c99c6e4169a660
Jeremy <jeremyfoo@gmail.com>
2010-09-04 03:02:52 +0800

    rudimentary retrival of dates

--- JONTUSemesterDates.m
+++ JONTUSemesterDates.m
@@ -37,79 +37,75 @@
 -(void)parse {
 	
 	NSMutableDictionary *sems = [NSMutableDictionary dictionary];
-	NSArray *semesterNames = [NSArray arrayWithObjects:@"SEMESTER 1", @"SEMESTER 2", @"SPECIAL TERM I", @"SPECIAL TERM II", nil];
+	NSArray *semesterNames = [NSArray arrayWithObjects:@"SEMESTER 1", @"SEMESTER 2", @"SPECIAL TERM II", @"SPECIAL TERM I", nil];
 	
 	NSString *toYearString = [NSString stringWithFormat:@"%i", year+1];
 	NSString *page = [[NSString alloc] initWithData:[self sendSyncXHRToURL:[NSURL URLWithString:[YEAR_URL stringByReplacingOccurrencesOfString:@"(year)" withString:[NSString stringWithFormat:@"%i-%@", year, [toYearString stringByMatching:@"([0-9][0-9])$" capture:1]]]] 
 																postValues:nil 
 																 withToken:NO] 
-										   encoding:NSUTF8StringEncoding];
+										   encoding:NSASCIIStringEncoding];
 	
 	page = [[page removeHTMLEntities] stringByReplacingOccurrencesOfRegex:@"[\\n|\\t|\\r]" withString:@""];
 	
-	NSArray *rows = [page captureComponentsMatchedByRegex:REGEX_TABLE_ROW];
-	
-	NSLog(@"%@",rows);
+	NSArray *rows = [page componentsMatchedByRegex:REGEX_TABLE_ROW];
 	
 	[page release];
 	
 	NSMutableDictionary *currentSemester = nil;
 	NSString *currentSemesterName = nil;
-	int previousFoundIndex = -1;
+	NSArray *rowcontents;
+	NSString *testSemName;
 	
 	for (int i=0;i<[rows count];i++) {
 		
-		NSArray *rowcontents = [[rows objectAtIndex:i] captureComponentsMatchedByRegex:REGEX_TABLE_CELL];
-		
-			
-		NSString *testSemName = [self matchedPrefixString:[[[rowcontents objectAtIndex:1] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] uppercaseString]
+		rowcontents = [[rows objectAtIndex:i] componentsMatchedByRegex:REGEX_TABLE_CELL];
+		testSemName = [self matchedPrefixString:[[[rowcontents objectAtIndex:0] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] uppercaseString]
 											inArray:semesterNames];
 		
-		// inspect for semester and setup
-		if (testSemName) {
-			if ((currentSemester) && (currentSemesterName)) {
-				[sems setObject:currentSemester forKey:currentSemesterName];
-			}				
+		if ([[[[rowcontents objectAtIndex:0] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] uppercaseString] hasPrefix:@"EVENTS"]) {
+			NSLog(@"%@", currentSemesterName);
+			[sems setObject:currentSemester forKey:currentSemesterName];
+			i = [rows count];
 			
-			[currentSemesterName release], currentSemesterName = nil;
-			currentSemesterName = testSemName;
-			
-			[currentSemester release], currentSemester = nil;
-			currentSemester = [NSMutableDictionary dictionary];
-			[currentSemester setObject:[[rowcontents objectAtIndex:2] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] forKey:@"SEM_START"];
-			[currentSemester setObject:[[rowcontents objectAtIndex:3] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] forKey:@"SEM_END"];
-			[currentSemester setObject:[[rowcontents objectAtIndex:4] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] forKey:@"SEM_DURATION"];
-			
-			previousFoundIndex = i;
-			
-			NSLog(@"Found %@", testSemName);
-			
-		} 
-		
-		if ((previousFoundIndex - i) == 0) {
-			// we already have a semster so we must parse data
-			[currentSemester setObject:[[rowcontents objectAtIndex:2] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] 
-								forKey:[NSString stringWithFormat:@"%@_START", [[[rowcontents objectAtIndex:1] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] uppercaseString]]];
-			[currentSemester setObject:[[rowcontents objectAtIndex:3] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] 
-								forKey:[NSString stringWithFormat:@"%@_END", [[[rowcontents objectAtIndex:1] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] uppercaseString]]];
-			[currentSemester setObject:[[rowcontents objectAtIndex:4] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] 
-								forKey:[NSString stringWithFormat:@"%@_DURATION", [[[rowcontents objectAtIndex:1] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] uppercaseString]]];
-			
-			previousFoundIndex = i;
-			
-			NSLog(@"Found %@", [[rowcontents objectAtIndex:1] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""]);
-			
-		}
+		} else {
+			// inspect for semester and setup
+			if (testSemName) {
+				if ((currentSemester) && (currentSemesterName)) {
+					[sems setObject:currentSemester forKey:currentSemesterName];
+				}				
 				
+				[currentSemesterName release], currentSemesterName = nil;
+				currentSemesterName = testSemName;
+				
+				[currentSemester release], currentSemester = nil;
+				currentSemester = [[NSMutableDictionary dictionary] retain];
+				[currentSemester setObject:[[rowcontents objectAtIndex:1] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] forKey:@"SEM_START"];
+				[currentSemester setObject:[[rowcontents objectAtIndex:2] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] forKey:@"SEM_END"];
+				[currentSemester setObject:[[rowcontents objectAtIndex:3] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] forKey:@"SEM_DURATION"];
+				
+				NSLog(@"Found %@", testSemName);
+				
+				rowcontents = [[rows objectAtIndex:++i] componentsMatchedByRegex:REGEX_TABLE_CELL];
+				
+			} 
+			
+			if ((currentSemester) && ([rowcontents count] == 4)) {
+				// we already have a semster so we must parse data
+				[currentSemester setObject:[[rowcontents objectAtIndex:1] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] 
+									forKey:[NSString stringWithFormat:@"%@_START", [[[rowcontents objectAtIndex:0] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] uppercaseString]]];
+				[currentSemester setObject:[[rowcontents objectAtIndex:2] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] 
+									forKey:[NSString stringWithFormat:@"%@_END", [[[rowcontents objectAtIndex:0] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] uppercaseString]]];
+				[currentSemester setObject:[[rowcontents objectAtIndex:3] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] 
+									forKey:[NSString stringWithFormat:@"%@_DURATION", [[[rowcontents objectAtIndex:0] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""] uppercaseString]]];
+				
+				NSLog(@"Found %@", [[rowcontents objectAtIndex:0] stringByReplacingOccurrencesOfRegex:REGEX_STRIP_HTMLTAGS withString:@""]);
+			}
+		}
 
 	}
+			
 	
-	if ((currentSemester) && (currentSemesterName)) {
-		[sems setObject:currentSemester forKey:currentSemesterName];
-	}
-		
-	
-	[semesters release];
+	[semesters release], semesters = nil;
 	semesters = [sems retain];
 	
 }
